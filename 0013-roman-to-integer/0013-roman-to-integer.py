a4a247aa6c60ab3cdd441e00e13ffa81849da765
Akhil Deshpande <68347350+adeshpande03@users.noreply.github.com>
2023-02-04 16:31:07 -0600

    Time: 64 ms (31.79%), Space: 13.9 MB (69.49%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,6 +1,6 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        romans = {
+        roman_to_integer = {
             'I': 1,
             'V': 5,
             'X': 10,
@@ -9,30 +9,5 @@ class Solution:
             'D': 500,
             'M': 1000,
         }
-        count = 0
-        while ('IV' in s or 'IX' in s or 'XL' in s or 'XC' in s or 'CD' in s or 'CM' in s) and len(s) > 0: 
-            if 'IV' in s:
-                count += 4
-                s = s.replace('IV', '')
-                print(s)
-            if 'IX' in s:
-                count += 9
-                s = s.replace('IX', '')
-                print(s)
-            if 'XL' in s:
-                count += 40
-                s = s.replace('XL', '')
-            if 'XC' in s:
-                count += 90
-                s = s.replace('XC', '')
-            if 'CD' in s:
-                count += 400
-                s = s.replace('CD', '')
-            if 'CM' in s:
-                count += 900
-                s = s.replace('CM', '')
-            
-        for i in s:
-            if i in romans:
-                count += romans[i]
-        return (count)
+        s = s.replace("IV", "IIII").replace("IX", "VIIII").replace("XL", "XXXX").replace("XC", "LXXXX").replace("CD", "CCCC").replace("CM", "DCCCC")
+        return sum(map(lambda x: roman_to_integer[x], s))
