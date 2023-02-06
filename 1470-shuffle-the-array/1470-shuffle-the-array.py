ddadf00aaee269a47f6efdbdb56bb4d0fa4148e3
Akhil Deshpande <68347350+adeshpande03@users.noreply.github.com>
2023-02-06 13:41:58 -0600

    Time: 72 ms (33.41%), Space: 14.1 MB (33.41%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.py
+++ 1470-shuffle-the-array/1470-shuffle-the-array.py
@@ -1,8 +1,5 @@
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
-        half = int(len(nums)/2)
-        il = nums[:half]
-        ul = nums[half:len(nums)]
-        fl = sum(map(list, list((zip(il, ul)))), [])
-        return (fl)
+        return sum(map(list, list((zip(nums[:len(nums)//2], nums[len(nums)//2:len(nums)])))), [])
+    
         
