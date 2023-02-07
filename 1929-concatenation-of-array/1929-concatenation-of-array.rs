f8af88211e4dcd30efe4fe9b8eb709432f662094
Akhil Deshpande <68347350+adeshpande03@users.noreply.github.com>
2023-02-07 13:21:51 -0600

    Time: 5 ms (53.77%), Space: 2.1 MB (89.45%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.rs
+++ 1929-concatenation-of-array/1929-concatenation-of-array.rs
@@ -1,12 +1,14 @@
 impl Solution {
-    pub fn get_concatenation(nums: Vec<i32>) -> Vec<i32> {
-        let mut nums = nums;
-        let mut v: Vec<i32> = nums.clone();
-        for i in v.iter()
-        {
-            nums.push(*i)
-        }
-        
-        nums
+    pub fn get_concatenation(mut nums: Vec<i32>) -> Vec<i32> {
+        let mut v = nums.clone();
+        v.append(&mut nums);
+        v
     }
 }
+// impl Solution {
+//     pub fn get_concatenation(mut nums: Vec<i32>) -> Vec<i32> {
+//         let mut result = nums.clone();
+//         result.append(&mut nums);
+//         result
+//     }
+// }
