b071b884fbffa566df9ee83eb96a082da3306ee0
Akhil Deshpande <68347350+adeshpande03@users.noreply.github.com>
2023-02-07 04:13:51 -0600

    Time: 0 ms (100.00%), Space: 2.1 MB (51.06%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.rs
@@ -0,0 +1,14 @@
+impl Solution {
+    pub fn shuffle(nums: Vec<i32>, n: i32) -> Vec<i32> {
+        let mut f: Vec<i32> = vec![];
+        
+        let firstSlice = &nums[0..nums.len() / 2];
+        let secondSlice = &nums[nums.len()/2..nums.len()];
+        for (x) in 0..n
+        { 
+            f.push(firstSlice[x as usize]);
+            f.push(secondSlice[x as usize]);
+        }
+        return f;
+    }
+}
