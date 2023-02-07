c35ff7fb382181c2ec369344e98426f0dce16f84
Akhil Deshpande <68347350+adeshpande03@users.noreply.github.com>
2023-02-07 13:32:44 -0600

    Time: 1 ms (73.81%), Space: 2.1 MB (64.29%) - LeetHub

---
+++ 2469-convert-the-temperature/2469-convert-the-temperature.rs
@@ -0,0 +1,8 @@
+impl Solution {
+    pub fn convert_temperature(celsius: f64) -> Vec<f64> {
+        let k = celsius + 273.15;
+        let f = celsius * 1.80 + 32.00;
+        let v = vec![k, f];
+        v
+    }
+}
