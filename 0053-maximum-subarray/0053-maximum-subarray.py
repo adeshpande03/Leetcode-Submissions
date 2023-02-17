c95f6b363ae6a73a83fb36eb2540c54fd4b44878
Akhil Deshpande <68347350+adeshpande03@users.noreply.github.com>
2023-02-17 16:53:20 -0600

    Time: 689 ms (94.40%), Space: 28.6 MB (41.15%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,17 +1,3 @@
-# class Solution:
-#     def maxSubArray(self, nums: List[int]) -> int:
-#         max_so_far =  float('inf') * -1
-#         max_ending_here = 0
-
-#         for i in range(len(nums)):
-#             max_ending_here = max_ending_here + nums[i]
-#             if (max_so_far < max_ending_here):
-#                 max_so_far = max_ending_here
-
-#             if max_ending_here < 0:
-#                 max_ending_here = 0
-#         return max_so_far
-
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         soFar = float('inf') * -1
@@ -20,6 +6,6 @@ class Solution:
             endHere = endHere + i
             if endHere > soFar:
                 soFar = endHere
-            if endHere < 0:
+            if endHere <= 0:
                 endHere = 0
         return soFar
