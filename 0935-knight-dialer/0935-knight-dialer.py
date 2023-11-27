5f523c6a324765b81b5f3a088bf6f1a90b4bc0dc
Akhil Deshpande <68347350+adeshpande03@users.noreply.github.com>
2023-11-27 16:32:49 -0600

    Time: 563 ms (83.28%), Space: 16.2 MB (91.15%) - LeetHub

---
+++ 0935-knight-dialer/0935-knight-dialer.py
@@ -0,0 +1,34 @@
+class Solution:
+    def knightDialer(self, n: int) -> int:
+        num_map = {0:[4,6], 
+                   1:[8,6], 
+                   2:[7,9], 
+                   3:[4,8], 
+                   4:[3,0,9], 
+                   5:[], 
+                   6:[1,7,0], 
+                   7:[2,6], 
+                   8:[1,3], 
+                   9:[4,2]}
+        MOD = 10**9 + 7
+        dp = [0] * 10
+        prev = [1] * 10
+        
+        for r in range(1,n):
+            dp = [0] * 10
+            for s in range(10):
+                ans = 0
+                for nsq in num_map[s]:
+                    ans = (ans + prev[nsq]) % MOD
+                dp[s] = ans
+            prev = dp
+            
+        ans = 0
+        for square in range(10):
+            ans = (ans + prev[square]) % MOD
+        return ans
+            
+            
+        
+        
+            
