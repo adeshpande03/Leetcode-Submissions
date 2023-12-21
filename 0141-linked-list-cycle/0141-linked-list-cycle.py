4df69c4b2e3eba208c32e002e3247cd1fada5c89
Akhil Deshpande <68347350+adeshpande03@users.noreply.github.com>
2023-12-21 07:38:11 +0530

    Time: 42 ms (99.58%), Space: 20.3 MB (89.53%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        if not head:
+            return None
+        seen = set()
+        while True:
+            if head.next == None:
+                return False
+            if head in seen:
+                return True
+            else:
+                seen.add(head)
+                head = head.next
+                
+        
