063c943b42c1d7e270d43b1e0e4b92122bb1e67d
Akhil Deshpande <68347350+adeshpande03@users.noreply.github.com>
2023-12-22 08:11:11 +0530

    Time: 34 ms (84.40%), Space: 17.8 MB (14.54%) - LeetHub

---
+++ 0293-flip-game/0293-flip-game.py
@@ -0,0 +1,11 @@
+class Solution:
+    def generatePossibleNextMoves(self, s: str) -> List[str]:
+        ans = []
+        if len(s) < 2:
+            return []
+        for i in range(len(s) - 1):
+            c = s[i: i + 2]
+            if '++' in c:
+                ans.append(s[:i] + '--' + s[i + 2:])
+        return ans
+                
