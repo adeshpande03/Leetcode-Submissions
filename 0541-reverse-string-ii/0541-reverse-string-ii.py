5080280858f91118b4f315327880aa7b47844cb1
Akhil Deshpande <68347350+adeshpande03@users.noreply.github.com>
2024-01-03 00:43:42 +0530

    Time: 65 ms (5.03%), Space: 17.6 MB (8.70%) - LeetHub

---
+++ 0541-reverse-string-ii/0541-reverse-string-ii.py
@@ -0,0 +1,8 @@
+class Solution:
+    def reverseStr(self, s: str, k: int) -> str:
+        l, r = 0, min(k, len(s))
+        while l < len(s):
+            s = s[:l] + s[l:r][::-1] + s[r:]
+            l += 2 * k
+            r = min(l + k, len(s))
+        return s
