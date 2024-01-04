09fbfec25caf0ad2800805d565e2b402c13a0c2e
Akhil Deshpande <68347350+adeshpande03@users.noreply.github.com>
2024-01-04 22:38:26 +0530

    Time: 559 ms (92.42%), Space: 31.8 MB (12.12%) - LeetHub

--- 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.py
+++ 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.py
@@ -8,8 +8,6 @@ class Solution:
             if value > 4:
                 return 1 + substract(value - 3)
         
-        hashmap = defaultdict(int)
-        for num in nums:
-            hashmap[num] += 1
+        hashmap = Counter(nums)
         
         return -1 if any([val == 1 for val in hashmap.values()]) else sum([substract(val) for val in hashmap.values()])
