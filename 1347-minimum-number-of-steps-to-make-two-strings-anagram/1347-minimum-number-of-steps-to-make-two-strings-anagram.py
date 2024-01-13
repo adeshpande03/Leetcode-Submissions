c31ad9ed4cc22be732967cd33179cd27fd9160f1
Akhil Deshpande <68347350+adeshpande03@users.noreply.github.com>
2024-01-12 19:11:30 -0600

    Time: 96 ms (96.31%), Space: 18.1 MB (5.24%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.py
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.py
@@ -2,7 +2,7 @@ class Solution:
     def minSteps(self, s: str, t: str) -> int:
         diff = Counter(s) - Counter(t)
         total = 0
-        for e in diff.values():
-            total += e
+        for e in diff:
+            total += diff[e]
 
         return total 
