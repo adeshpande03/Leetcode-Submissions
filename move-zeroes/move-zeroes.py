fd2f91f5f103a36293bb4cbb925996a14a8f0dad
Akhil Deshpande <68347350+adeshpande03@users.noreply.github.com>
2024-01-13 14:37:59 -0600

    Time: 125 ms, Memory: 18.9 MB - LeetHub

---
+++ move-zeroes/move-zeroes.py
@@ -0,0 +1,6 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        ogLen = len(nums)
+        nums[:] = [d for d in nums if d != 0]
+        for i in range(ogLen - len(nums)):
+            nums.append(0)
