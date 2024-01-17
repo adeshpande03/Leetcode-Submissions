6a25ad5b8e9ce5678561137d59dad22ab86e2378
Akhil Deshpande <68347350+adeshpande03@users.noreply.github.com>
2024-01-17 11:59:09 -0600

    Time: 1 ms (61.71%), Space: 41.3 MB (38.29%) - LeetHub

--- 1119-remove-vowels-from-a-string/1119-remove-vowels-from-a-string.java
+++ 1119-remove-vowels-from-a-string/1119-remove-vowels-from-a-string.java
@@ -1,14 +1,7 @@
 class Solution {
     public String removeVowels(String s) {
-        String c = new String();
-        String v = "aeiou";
-        for (int i = 0; i < s.length(); i++)
-        {
-            if (v.indexOf(s.charAt(i)) == -1)
-            {
-                c = c + s.charAt(i);
-            }
-        }
-        return c;
+   String s1;
+   s1=s.replaceAll("[aeiou]","");
+   return s1;
     }
 }
