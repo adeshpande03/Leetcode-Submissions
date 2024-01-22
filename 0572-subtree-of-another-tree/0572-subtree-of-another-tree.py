a06750a617387c4c19938d947ccc3083ddae9b45
Akhil Deshpande <68347350+adeshpande03@users.noreply.github.com>
2024-01-22 13:24:28 -0600

    Time: 82 ms (68.72%), Space: 16.8 MB (98.1%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -9,4 +9,4 @@ class Solution:
     def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
         if p and q:
             return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
-        return p is q
+        return p == q
