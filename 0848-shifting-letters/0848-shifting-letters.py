fa1365c5ec2a765745e1feeb556a82dea6e0af0b
Akhil Deshpande <68347350+adeshpande03@users.noreply.github.com>
2024-01-23 15:02:53 -0600

    Time: 552 ms (98.51%), Space: 29.6 MB (90.67%) - LeetHub

---
+++ 0848-shifting-letters/0848-shifting-letters.py
@@ -0,0 +1,11 @@
+class Solution:
+    def shiftingLetters(self, s: str, shifts: List[int]) -> str:
+        shifts = [i % 26 for i in list(accumulate(shifts[::-1]))][::-1]
+        s = list(s)
+        for i in range(len(s)):
+            d = (ord(s[i]) + shifts[i])
+            if d > 122:
+                d -= 26
+            s[i] = chr(d)
+        print(shifts)
+        return ''.join(s)
