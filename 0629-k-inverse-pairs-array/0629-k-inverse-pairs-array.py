a6ad7dec5da1849a4b34bddce19ee5ce6b510700
Akhil Deshpande <68347350+adeshpande03@users.noreply.github.com>
2024-01-26 20:14:41 -0600

    Time: 173 ms (92.14%), Space: 16.7 MB (87.14%) - LeetHub

--- 0629-k-inverse-pairs-array/0629-k-inverse-pairs-array.py
+++ 0629-k-inverse-pairs-array/0629-k-inverse-pairs-array.py
@@ -1,13 +1,28 @@
 class Solution:
     def kInversePairs(self, n: int, k: int) -> int:
-        @cache
-        def f(n, k):
-            if k == 0:
-                return 1
-            if k < 0 or n <= 0:
-                return 0
-            return (f(n - 1, k) + f(n, k - 1) - f(n - 1, k - n) + 10**9 + 7) % (
-                10**9 + 7
-            )
+        M = 1000000007
+        if k == 0:
+            return 1
+        if n == 1:
+            return 1 if k == 0 else 0
 
-        return f(n, k)
+        current_count = [0] * (k + 1)
+        current_count[0] = 1
+        previous_count = [0] * (k + 1)
+
+        for i in range(n - 2, -1, -1):
+            total_count = 0
+            threshold = n - i
+
+            for j in range(0, k + 1):
+                total_count += current_count[j]
+
+                if j >= threshold:
+                    total_count -= current_count[j - threshold]
+
+                total_count %= M
+                previous_count[j] = total_count
+
+            current_count, previous_count = previous_count, current_count
+
+        return current_count[k]
