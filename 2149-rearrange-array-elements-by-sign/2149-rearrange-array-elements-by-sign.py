b751941af7fc463baa276fd5791fe7ff9e0d86bc
Akhil Deshpande <68347350+adeshpande03@users.noreply.github.com>
2024-02-14 10:02:01 -0600

    Time: 1053 ms (32.13%), Space: 47.2 MB (74.45%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
@@ -1,25 +1,15 @@
 class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
-        a, b, c = [], [], []
-
-        # Separate positive and negative integers into two separate arrays
+        a, b, ans = [], [], []
         for num in nums:
             if num > 0:
                 a.append(num)
             else:
                 b.append(num)
-
-        # Interleave positive and negative integers while preserving their order
-        count, c1, c2 = 0, 0, 0
-        for num in nums:
-            if count % 2 == 0:
-                n = a[c1]
-                c.append(n)
-                c1 += 1
+        
+        for i in range(len(nums)):
+            if not i % 2:
+                ans.append(a[i//2])
             else:
-                n = b[c2]
-                c.append(n)
-                c2 += 1
-            count += 1
-
-        return c
+                ans.append(b[i//2])
+        return ans
