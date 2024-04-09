935ebe3283121aae8a57a63181b04b0ec5eff03f
Akhil Deshpande <68347350+adeshpande03@users.noreply.github.com>
2024-04-08 19:58:53 -0500

    Time: 37 ms (88.06%), Space: 16.5 MB (92.6%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.py
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.py
@@ -1,3 +1,10 @@
 class Solution:
     def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
-        return sum(min(tickets[k] - (i > k), num) for i, num in enumerate(tickets))
+        ans = 0
+
+        for i, v in enumerate(tickets):
+            if i <= k:
+                ans += min(tickets[i], tickets[k])
+            else:
+                ans += min(tickets[i], tickets[k] - 1)
+        return ans
