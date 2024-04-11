cb2686911ff8133fd61258e78cb9a6c39ada7d4a
Akhil Deshpande <68347350+adeshpande03@users.noreply.github.com>
2024-04-11 14:30:28 -0500

    Time: 747 ms (12.96%), Space: 30.7 MB (64.68%) - LeetHub

--- 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.py
+++ 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.py
@@ -3,6 +3,6 @@ class Solution:
         nums.sort()
         il = [nums[k:k + 3] for k in range(0, len(nums), 3)]
         for test in il:
-            if abs(test[-1] - test[0]) > k or abs(test[1] - test[0]) > k:
+            if abs(test[-1] - test[0]) > k:
                 return []
         return il
