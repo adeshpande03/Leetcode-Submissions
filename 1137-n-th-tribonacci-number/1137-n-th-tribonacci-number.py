360ab7ddc110970d494d39b7014ff123f489a8a3
Akhil Deshpande <68347350+adeshpande03@users.noreply.github.com>
2024-04-23 19:06:55 -0500

    Time: 40 ms (18.45%), Space: 16.5 MB (85.24%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -5,6 +5,6 @@ class Solution:
         if n <= 2:
             return l[n]
         for _ in range(n - 2):
-            l.append(sum(l[-3:]))
+            l.append(sum(l))
             l = l[1:]
         return l[-1]
