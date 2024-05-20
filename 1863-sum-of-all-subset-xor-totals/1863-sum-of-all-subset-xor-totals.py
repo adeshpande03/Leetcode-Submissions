5a9c84e78a9f29d266301201503dbb8c58748e85
Akhil Deshpande <68347350+adeshpande03@users.noreply.github.com>
2024-05-19 22:34:07 -0500

    Time: 26 ms (98.96%), Space: 16.5 MB (48.58%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.py
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.py
@@ -1,17 +1,6 @@
 class Solution:
     def subsetXORSum(self, nums: List[int]) -> int:
-        ans = 0
-        
-        def backtrack(start,curr):
-            nonlocal ans
-            ans+=curr
-            if start==len(nums):
-                return
-
-            for i in range(start,len(nums)):
-                curr=curr^nums[i]
-                backtrack(i+1,curr)
-                curr = curr^nums[i]
-
-        backtrack(0,0)
-        return ans
+        res = 0
+        for num in nums:
+            res |= num
+        return res * (1 << (len(nums) - 1))
