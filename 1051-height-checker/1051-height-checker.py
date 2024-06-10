0289ac015bca70235d8c7235b9811d066f06336d
Akhil Deshpande <68347350+adeshpande03@users.noreply.github.com>
2024-06-10 03:49:59 +0100

    Time: 36 ms (77.24%), Space: 16.4 MB (97.08%) - LeetHub

--- 1051-height-checker/1051-height-checker.py
+++ 1051-height-checker/1051-height-checker.py
@@ -1,16 +1,10 @@
 class Solution:
     def heightChecker(self, heights: List[int]) -> int:
-        he = heights[:]
-        c = 0
-        h = 1
-        while h:
-            h = 0
-            for i in range(len(heights) - 1):
-                if heights[i + 1] < heights[i]:
-                    h = 1
-                    heights[i], heights[i + 1] = heights[i + 1], heights[i]
-        for i in range(len(he)):
-            if he[i] != heights[i]:
-                c += 1
-        return c
-        print(heights)
+        c=heights[:]
+        d=0
+        c.sort()
+        for i in range(len(c)):
+            if heights[i]!=c[i]:
+                d+=1
+        return d
+        
