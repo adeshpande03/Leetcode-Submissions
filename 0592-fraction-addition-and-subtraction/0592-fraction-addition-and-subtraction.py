12760f84ee10fa70ecf27c4308061f8a578dde94
Akhil Deshpande <68347350+adeshpande03@users.noreply.github.com>
2024-08-23 00:05:50 -0500

    Time: 33 ms (76.74%), Space: 16.5 MB (89.53%) - LeetHub

--- 0592-fraction-addition-and-subtraction/0592-fraction-addition-and-subtraction.py
+++ 0592-fraction-addition-and-subtraction/0592-fraction-addition-and-subtraction.py
@@ -3,14 +3,7 @@ from fractions import Fraction
 
 
 class Solution:
-	"""
-	Time:   O(n)
-	Memory: O(n)
-	"""
-
 	FRACTION_PATTERN = r'([+-]?[^+-]+)'
-	
-	# First solution
 	def fractionAddition(self, expression: str) -> str:
 		result_fraction = Fraction(0)
 
@@ -18,12 +11,4 @@ class Solution:
 			n, d = map(int, exp.split('/'))
 			result_fraction += Fraction(n, d)
 
-		return f'{result_fraction.numerator}/{result_fraction.denominator}'
-
-	# Second solution
-	def fractionAddition(self, expression: str) -> str:
-		result_fraction = sum(
-			(Fraction(*map(int, exp.split('/'))) for exp in re.findall(self.FRACTION_PATTERN, expression)),
-			Fraction(0)
-		)
 		return f'{result_fraction.numerator}/{result_fraction.denominator}'
