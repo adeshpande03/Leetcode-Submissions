21bc467c9402f09f3168d2fd39956e8e10dc9e21
Akhil Deshpande <68347350+adeshpande03@users.noreply.github.com>
2025-11-30 16:02:33 -0600

    Time: 0 ms (100%), Space: 17.6 MB (100%) - LeetHub

--- 2409-count-days-spent-together/2409-count-days-spent-together.py
+++ 2409-count-days-spent-together/2409-count-days-spent-together.py
@@ -1,14 +1,15 @@
 class Solution:
-    def countDaysTogether(self, arriveAlice: str, leaveAlice: str, arriveBob: str, leaveBob: str) -> int:
-        monthdays = [-1, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    def countDaysTogether(self, arriveAlice: str, leaveAlice: str,
+                                arriveBob: str,   leaveBob: str) -> int:
+
+        d = {1:0,   2:31,  3:59,   4:90,   5:120,  6:151,         
+             7:181, 8:212, 9:243, 10:273, 11:304, 12:334}         
+
+        def days(date):                                           
+            mon, day = date.split('-')                            
+            return d[int(mon)]+int(day)                           
+
+        arrive, leave = (max(days(arriveAlice),days(arriveBob)),  
+                         min(days(leaveAlice ),days(leaveBob )))  
         
-        start, end = max(arriveAlice, arriveBob), min(leaveAlice, leaveBob)
-        
-        start_month, start_day = int(start[:2]), int(start[3:])
-        end_month, end_day = int(end[:2]), int(end[3:])
-        
-        if start > end: return 0
-        elif start_month == end_month: 
-            return end_day - start_day + 1
-        else: 
-            return (monthdays[start_month] - start_day) + sum([monthdays[i] for i in range(start_month + 1, end_month)]) + end_day + 1
+        return  max(leave-arrive+1,0)                            
